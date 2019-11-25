beef88729dfd35216b5832a0f15e19048bb1aab6
kris-reinhardt <54648029+kris-reinhardt@users.noreply.github.com>
2019-11-25 08:37:26 -0500

    Create HAITI_dataTable_urgences.sql

---
+++ haiti-indicators/HAITI_dataTable_urgences.sql
@@ -0,0 +1,31 @@
+/*ABOUT
+ * The visits data table is for counting the number of emergency visits recorded in the EMR. 
+ * Each row of the query represents a visit.
+ 
+ * Variables: patient id, visit type, visit start date, visit end date, length of stay (days), sex, address fields, age (not yet available)
+ * Possible indicators: count of visits (admissions or exits) by type, average length of stay
+ * Possible disaggregation: sex, address fields, age (not yet available)
+ * Customization: address variables and address coulmn names (rows 24 & 25), visit type name (row 30)*/
+ 
+SELECT
+    pvdd.patient_id AS "patient id",
+    pvdd.visit_type_name AS "visit type", 
+    pvdd.visit_start_date AS "visit start date",
+	pvdd.visit_end_date AS "visit end date",
+/*Length of stay in days calculates the number of days between admission and discharge. Admission and discharge on the same day is counted as 1 day*/
+	CASE
+		WHEN pvdd.visit_end_date::date - pvdd.visit_start_date::date = 0 THEN 1
+		ELSE pvdd.visit_end_date::date - pvdd.visit_start_date::date
+	END AS "length of stay (days)",
+/*Age calcuated in Metabase is an estimate using January 1st of the birthyear and the start date of the visit*/
+    /*(date_part('year', age(pvdd.visit_start_date, to_date((concat(piv.birthyear,'-01-01')), 'YYYY-MM-DD'))))::int AS "age at visit start",*/
+    piv.gender AS "sex",
+/*The address variables (address1, address2, etc.) and address column names (region, commune, etc.) should be customized according to the address hierarchy used.*/
+    piv.address4 AS "department", 
+    piv.address3 AS "commune"
+FROM patient_visit_details_default AS pvdd
+LEFT OUTER JOIN patient_information_view AS piv
+    ON pvdd.patient_id = piv.patient_id
+/*The visit_type_name can be specified to display only specified visit types.*/
+WHERE pvdd.visit_type_name = 'Urgences'
+ORDER BY pvdd.patient_id, pvdd.visit_start_date
