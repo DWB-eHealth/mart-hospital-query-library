1206d3693027294a2557d2ce2a7db657f5319607
Kris <54648029+kris-reinhardt@users.noreply.github.com>
2020-01-03 15:52:09 -0500

    Update admissionTotale_dataVer_Dec2019.sql

--- haiti-indicators/data-verification-queries/admissionTotale_dataVer_Dec2019.sql
+++ haiti-indicators/data-verification-queries/admissionTotale_dataVer_Dec2019.sql
@@ -23,20 +23,11 @@ SELECT
 	pi."Patient_Identifier",
 	ad.location	AS admission_location,
 	ad.admission_date,
-	dd.location	AS discharge_location,
-	dd.discharge_date,
-	CASE
-		WHEN dd.discharge_date::date IS NULL THEN NULL 
-		WHEN dd.discharge_date::date - ad.admission_date::date = 0 THEN 1
-		ELSE dd.discharge_date::date - ad.admission_date::date
-	END AS "length of stay (days)",
 	ad.gender,
 	ad.age_at_bed_assignment AS age_at_admission,
 	piv.address4 AS "department", 
     piv.address3 AS "commune"
 FROM admission_date AS ad 
-LEFT OUTER JOIN discharge_date AS dd
-	ON ad.patient_id = dd.patient_id AND ad.visit_id = dd.visit_id
 LEFT OUTER JOIN patient_information_view AS piv
     ON ad.patient_id = piv.patient_id
 LEFT OUTER JOIN patient_identifier AS pi
