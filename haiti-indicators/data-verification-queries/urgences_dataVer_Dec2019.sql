d284bdaf4ed15773320712c5bee88e1752c40a35
Kris <54648029+kris-reinhardt@users.noreply.github.com>
2020-01-06 13:42:04 -0500

    Update urgences_dataVer_Dec2019.sql

--- haiti-indicators/data-verification-queries/urgences_dataVer_Dec2019.sql
+++ haiti-indicators/data-verification-queries/urgences_dataVer_Dec2019.sql
@@ -1,6 +1,6 @@
 SELECT
     pi."Patient_Identifier",
-	pvdd.patient_id AS "patient id",
+    pvdd.patient_id AS "patient id",
     pvdd.visit_type_name AS "visit type", 
     pvdd.visit_start_date AS "visit start date",
     pvdd.visit_end_date AS "visit end date",
@@ -11,6 +11,6 @@ FROM patient_visit_details_default AS pvdd
 LEFT OUTER JOIN patient_information_view AS piv
     ON pvdd.patient_id = piv.patient_id
 LEFT OUTER JOIN patient_identifier AS pi
-    on pvdd.patient_id = pi.patient_id
+    ON pvdd.patient_id = pi.patient_id
 WHERE pvdd.visit_type_name = 'Urgences' and pvdd.visit_start_date::date >= '2019-12-01' and pvdd.visit_start_date::date < '2020-01-01'
 ORDER BY pvdd.patient_id, pvdd.visit_start_date
