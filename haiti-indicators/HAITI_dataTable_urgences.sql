8a0db541a8fa342cf2aee1db58b14639d0a577c4
kris-reinhardt <54648029+kris-reinhardt@users.noreply.github.com>
2020-01-29 08:50:07 -0500

    Update HAITI_dataTable_urgences.sql
    Added age from the person_details_default table. Modified description of query.

--- haiti-indicators/HAITI_dataTable_urgences.sql
+++ haiti-indicators/HAITI_dataTable_urgences.sql
@@ -1,10 +1,11 @@
 /*ABOUT
- * The visits data table is for counting the number of emergency visits recorded in the EMR. 
+ * The visits data table is for counting the number of visits recorded in the EMR.
+ * This query has been customized to only count 'Urgences' visit types.
  * Each row of the query represents a visit.
  
- * Variables: patient id, visit type, visit start date, visit end date, length of stay (days), sex, address fields, age (not yet available)
+ * Variables: patient id, visit type, visit start date, visit end date, length of stay (days), age, sex, address fields
  * Possible indicators: count of visits (admissions or exits) by type, average length of stay
- * Possible disaggregation: sex, address fields, age (not yet available)
+ * Possible disaggregation: age, sex, address fields
  * Customization: address variables and address coulmn names (rows 24 & 25), visit type name (row 30)*/
  
 SELECT
@@ -17,8 +18,7 @@ SELECT
 		WHEN pvdd.visit_end_date::date - pvdd.visit_start_date::date = 0 THEN 1
 		ELSE pvdd.visit_end_date::date - pvdd.visit_start_date::date
 	END AS "length of stay (days)",
-/*Age calcuated in Metabase is an estimate using January 1st of the birthyear and the start date of the visit*/
-    /*(date_part('year', age(pvdd.visit_start_date, to_date((concat(piv.birthyear,'-01-01')), 'YYYY-MM-DD'))))::int AS "age at visit start",*/
+    pdd.age AS "age in years",
     piv.gender AS "sex",
 /*The address variables (address1, address2, etc.) and address column names (region, commune, etc.) should be customized according to the address hierarchy used.*/
     piv.address4 AS "department", 
@@ -26,6 +26,8 @@ SELECT
 FROM patient_visit_details_default AS pvdd
 LEFT OUTER JOIN patient_information_view AS piv
     ON pvdd.patient_id = piv.patient_id
+LEFT OUTER JOIN person_details_default AS pdd
+	ON pvdd.patient_id = pdd.person_id 
 /*The visit_type_name can be specified to display only specified visit types.*/
 WHERE pvdd.visit_type_name = 'Urgences'
 ORDER BY pvdd.patient_id, pvdd.visit_start_date
