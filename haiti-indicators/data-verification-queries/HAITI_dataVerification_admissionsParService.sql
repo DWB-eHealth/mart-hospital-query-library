b098ef376dd1c9ce1c7ba0a6b0f6ad23b83c83cf
kris-reinhardt <54648029+kris-reinhardt@users.noreply.github.com>
2020-01-30 09:29:35 -0500

    Update HAITI_dataVerification_admissionsParService.sql

--- haiti-indicators/data-verification-queries/HAITI_dataVerification_admissionsParService.sql
+++ haiti-indicators/data-verification-queries/HAITI_dataVerification_admissionsParService.sql
@@ -14,19 +14,19 @@ SELECT
 /*Age calcuated in Metabase is an estimate using January 1st of the birthyear and the start date of the visit.*/
 	CASE
 		WHEN pdd.birthyear IS NOT NULL THEN (date_part('year', age(bmlv.start_date, to_date((concat(pdd.birthyear,'-01-01')), 'YYYY-MM-DD'))))::int
-		ELSE NULL 
+		ELSE NULL
 	END AS "age at admission",
 	piv.gender AS "sex",
 /*The address variables (address1, address2, etc.) and address column names (region, commune, etc.) should be customized according to the address hierarchy used.*/
-    piv.address4 AS "department", 
-    piv.address3 AS "commune"
+	piv.address4 AS "department", 
+	piv.address3 AS "commune"
 FROM bed_management_locations_view AS bmlv
 LEFT OUTER JOIN patient_information_view AS piv
-    ON bmlv.patient_id = piv.patient_id
-LEFT OUTER JOIN person_details_default AS pdd 
+	ON bmlv.patient_id = piv.patient_id
+LEFT OUTER JOIN person_details_default AS pdd
 	ON bmlv.patient_id = pdd.person_id 
 LEFT OUTER JOIN patient_identifier AS pi
-    on bmlv.patient_id = pi.patient_id
+	ON bmlv.patient_id = pi.patient_id
 /*The date range specified below can be changed depending on which reporting period needs to be verified.*/	
 WHERE bmlv.start_date::date >= '2020-01-01' and bmlv.start_date::date < '2020-02-01'
 ORDER BY bmlv.patient_id, bmlv.start_date
