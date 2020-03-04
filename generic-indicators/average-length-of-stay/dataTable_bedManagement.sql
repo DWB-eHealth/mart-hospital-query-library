1bd5f4c6c7902b91fa223ef3e579a8a5c07fdf38
kris-reinhardt <54648029+kris-reinhardt@users.noreply.github.com>
2020-03-04 14:48:24 -0500

    update alos folder

--- generic-indicators/average-length-of-stay/dataTable_bedManagement.sql
+++ generic-indicators/average-length-of-stay/dataTable_bedManagement.sql
@@ -2,17 +2,6 @@
  * The bed management data table is for counting the number of admissions to each inpatient department or service. 
  * Each row of the query represents the admission of a patient to a particular department or service. 
  * If a patient is admitted to multiple services during the visit, then there will be one row for each admission.
- 
- * Variables: patient id, service, service admission date, service discharge date, length of stay (days), age at admission, sex, and address fields
- * Possible indicators: count of admissions by service, count of exits by service, average length of stay by service
- * Possible disaggregation: age groups, sex, address fields
- * Customization: address variables and address coulmn names (rows 25 & 26)*/
-
-SELECT
-	bmlv.patient_id AS "patient id",
-	bmlv.location AS "Service",
-	bmlv.start_date AS "service admission date", 
-	bmlv.discharge_date AS "service discharge date",
 /*Length of stay in days calculates the number of days between admission and discharge. Admission and discharge on the same day is counted as 1 day.*/
 	CASE
 		WHEN bmlv.discharge_date::date - bmlv.start_date::date = 0 THEN 1
