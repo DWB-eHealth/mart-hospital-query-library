42649208e7e50bbcd65fe4ecc4ef4d9825b9fb5d
kris-reinhardt <54648029+kris-reinhardt@users.noreply.github.com>
2020-03-26 11:20:50 -0400

    Update iq_dataTable_MLOVisit.sql
    Update simplifies and fixes issues that occured when adding 'visit id' to Mart form tables. 'About' section also updated.

--- iq-indicators/iq_dataTable_MLOVisit.sql
+++ iq-indicators/iq_dataTable_MLOVisit.sql
@@ -1,6 +1,7 @@
 /*ABOUT
-* The MLO + Admission Committee data table connects information enter in the two forms together.
-* Each row represents a MLO encounter and the admission committee data if availble.
+* The MLO + Admission Committee data table connects information enter in the MLO Medical Assessment and the Admission Committee forms.
+* This query assumes that matching MLO and admission committee forms are completed in the same visit.
+* Each row represents a visit where the MLO Medical Assessment was completed. If the Admission Committee form was also completed in that visit, it is connected to the MLO form. 
 * If a patient has been assessed by an MLO and/or reviewed by the admission committee more than once, there will be multiple lines for this patient.
 
 * Variables: patient ID, sex, date of MLO assessment, type of MLO assessment, hospital of origin, date of injury, days between injury and MLO assessment, cause of injury, date of injury, date of last surgery, days between last surgery and MLO assessment, date of presentation to admission committee, outcome of admission committee, requested location of admission, reason for refusal
@@ -8,20 +9,6 @@
 * Possible disaggregation: sex
 * Customization: none */
 
-WITH mlo_assessment AS (
-	SELECT 
-		mma.*,
-		pedd.visit_id 
-	FROM mlo_medical_assessment AS mma
-	LEFT OUTER JOIN patient_encounter_details_default AS pedd
-		ON mma.encounter_id = pedd.encounter_id),
-adm_committee AS (
-	SELECT 
-		ac.*,
-		pedd.visit_id 
-	FROM admission_committee AS ac
-	LEFT OUTER JOIN patient_encounter_details_default AS pedd 
-		ON ac.encounter_id = pedd.encounter_id)
 SELECT 
 	mlo.patient_id,
 	mlo.visit_id,
@@ -38,9 +25,9 @@ SELECT
 	adm.outcome_of_admission_committee, 
 	adm.requested_admission, 
 	adm.reason_for_refusal
-FROM mlo_assessment AS mlo
+FROM mlo_medical_assessment AS mlo
 LEFT OUTER JOIN patient_information_view AS piv 
 	ON mlo.patient_id = piv.patient_id
-LEFT OUTER JOIN adm_committee AS adm
+LEFT OUTER JOIN admission_committee AS adm
 	ON mlo.visit_id = adm.visit_id
-ORDER BY mlo.date_of_assessment::timestamp  
+ORDER BY mlo.date_of_assessment::timestamp
