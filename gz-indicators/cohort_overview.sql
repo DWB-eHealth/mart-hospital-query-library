5cd8771835a13bc09fc01b10d65442ae33292dbd
kris-reinhardt <54648029+kris-reinhardt@users.noreply.github.com>
2021-12-23 21:28:00 -0500

    Update cohort_overview.sql

--- gz-indicators/cohort_overview.sql
+++ gz-indicators/cohort_overview.sql
@@ -16,7 +16,7 @@ cte_initial_medical_assessment AS (
 		DISTINCT ON (patient_program_id) patient_program_id,
 		type_of_injury,
 		date_of_injury, 
-		cause_of_injury, 
+		cause_of_injury,
 		cause_of_burn, 
 		location_of_first_treatment, 
 		total_tbsa 
@@ -33,14 +33,47 @@ SELECT
 		WHEN ppdd.age_during_enrollment::int >=45 THEN '>45'
 		ELSE NULL 
 	END AS age_group_during_enrollment,
-	pdd.gender,
+	CASE 
+		WHEN pdd.gender = 'M' THEN 'Male'
+		WHEN pdd.gender = 'F' THEN 'Female'
+		ELSE NULL 
+	END AS sex,
 	CASE 
 		WHEN ppdd.program_id = 1 THEN 'Trauma'
 		WHEN ppdd.program_id = 2 THEN 'Burn'
 	END AS program_name,
 	ppdd.date_enrolled,
 	ppdd.date_completed,
-	ppdd.program_outcome
+	ppdd.program_outcome,
+	abs(ppdd.date_completed::date - ppdd.date_enrolled::date) AS los_days,
+	cfc.location_name,
+	cfc.source_of_initial_patient_referral,
+	cfc.type_of_injury AS type_of_injury_fc,
+	cfc.admission_status,
+	cfc.reason_of_rejection,
+	cfc.out_of_criteria,
+	cfc.patient_sent_to_another_actor,
+	cfc.referral_location,
+	cima.type_of_injury AS type_of_injury_ima,
+	cima.date_of_injury, 
+	abs(ppdd.date_enrolled::date - cima.date_of_injury::date) AS day_diff_injury_admission,
+	cima.cause_of_injury, 
+	cima.cause_of_burn, 
+	cima.location_of_first_treatment, 
+	cima.total_tbsa, 
+	CASE 
+		WHEN cima.total_tbsa <10 THEN '0.1-9.9%'
+		WHEN cima.total_tbsa >=10 AND cima.total_tbsa <20 THEN '10-19.9%'
+		WHEN cima.total_tbsa >=20 AND cima.total_tbsa <30 THEN '20-29.9%'
+		WHEN cima.total_tbsa >=30 AND cima.total_tbsa <40 THEN '30-39.9%'
+		WHEN cima.total_tbsa >=40 AND cima.total_tbsa <50 THEN '40-49.9%'
+		WHEN cima.total_tbsa >=50 AND cima.total_tbsa <60 THEN '50-59.9%'
+		WHEN cima.total_tbsa >=60 AND cima.total_tbsa <70 THEN '60-69.9%'
+		WHEN cima.total_tbsa >=70 AND cima.total_tbsa <80 THEN '70-79.9%'
+		WHEN cima.total_tbsa >=80 AND cima.total_tbsa <90 THEN '80-89.9%'
+		WHEN cima.total_tbsa >89.9 THEN '>=90%'
+		ELSE NULL 
+	END AS tbsa_categories
 FROM patient_program_data_default ppdd 
 LEFT OUTER JOIN person_details_default pdd 
 	ON ppdd.patient_id = pdd.person_id
