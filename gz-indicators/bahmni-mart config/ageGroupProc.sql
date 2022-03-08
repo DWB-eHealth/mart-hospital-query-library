706ecbfb3da5486c6321c6ad3d7ec0737e129e3a
Kris <54648029+kris-reinhardt@users.noreply.github.com>
2022-03-08 11:51:48 -0500

    testing label change for ordering

--- gz-indicators/bahmni-mart config/ageGroupProc.sql
+++ gz-indicators/bahmni-mart config/ageGroupProc.sql
@@ -7,7 +7,7 @@ BEGIN
   IF EXTRACT(YEAR FROM (age(process_start_date, birthdate))) BETWEEN 0 AND 4
   THEN RETURN '0-5';
   ELSEIF EXTRACT(YEAR FROM (age(process_start_date, birthdate))) BETWEEN 5 AND 14
-    THEN RETURN '5-15';
+    THEN RETURN '05-15';
   ELSEIF (EXTRACT(YEAR FROM (age(process_start_date, birthdate))) BETWEEN 15 AND 44)
     THEN RETURN '15-45';
   ELSEIF EXTRACT(YEAR FROM (age(process_start_date, birthdate))) >= 45
