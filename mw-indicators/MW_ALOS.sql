cf65c5c24c67e1da26ddcb633b8d3d03d353ff18
kris-reinhardt <54648029+kris-reinhardt@users.noreply.github.com>
2022-11-17 15:07:53 +0100

    Create MW_ALOS.sql

---
+++ mw-indicators/MW_ALOS.sql
@@ -0,0 +1,30 @@
+/*ABOUT
+ * The visits data table is for counting the number of consultations/admissions/exits to or from a particular department (Emergency, OPD, IPD etc.). 
+ * Each row of the query represents a visit.
+ 
+ * Variables: patient id, visit type, visit start date, visit end date, length of stay (days), age at visit start, sex, address fields
+ * Possible indicators: count of visits (admissions or exits) by type, average length of stay
+ * Possible disaggregation: age groups, sex, address fields
+ * Customization: address variables and address coulmn names (rows 24 & 25)*/
+ 
+SELECT
+    pvdd.patient_id AS "patient id",
+    pvdd.visit_type_name AS "visit type", 
+    pvdd.visit_start_date AS "visit start date",
+	pvdd.visit_end_date AS "visit end date",
+/*Length of stay in days calculates the number of days between admission and discharge. Admission and discharge on the same day is counted as 1 day*/
+	CASE
+		WHEN pvdd.visit_end_date::date - pvdd.visit_start_date::date = 0 THEN 1
+		ELSE pvdd.visit_end_date::date - pvdd.visit_start_date::date
+	END AS "length of stay (days)",
+/*Age calcuated in Metabase is an estimate using January 1st of the birthyear and the start date of the visit*/
+    (date_part('year', age(pvdd.visit_start_date, to_date((concat(piv.birthyear,'-01-01')), 'YYYY-MM-DD'))))::int AS "age at visit start",
+    piv.gender AS "sex",
+/*The address variables (address1, address2, etc.) and address column names (region, commune, etc.) should be customized according to the address hierarchy used.*/
+    piv.address4 AS traditional_authority_town, 
+    piv.address3 AS district
+FROM patient_visit_details_default AS pvdd
+LEFT OUTER JOIN patient_information_view AS piv
+    ON pvdd.patient_id = piv.patient_id
+WHERE pvdd.visit_type_name = 'IPD'
+ORDER BY pvdd.patient_id, pvdd.visit_start_date
